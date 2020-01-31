0d4cf94a142e0219558e23b54fcc3fb89113dcbf
prashdeep <pradeep.kumar44@gmail.com>
2020-01-31 15:04:25 +0530

    committing the organization service

--- src/main/java/bt/organizationservice/controller/OrganizationController.java
+++ src/main/java/bt/organizationservice/controller/OrganizationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/organization")
+@RequestMapping("/api/v1/organization")
 public class OrganizationController {
 
     @GetMapping("/{id}")
